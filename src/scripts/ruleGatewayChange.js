9c8efc0d94459fa446e05bb8872bfd57a593d51c
Nathan Banks <nathantb3@gmail.com>
2020-09-20 15:17:05 +0100

    refactor: Improve selected gateway logging

--- src/scripts/ruleGatewayChange.js
+++ src/scripts/ruleGatewayChange.js
@@ -11,19 +11,17 @@ const run = async (page, pfSenseIP, ruleID, gatewayText) => {
     await page.waitFor(1000);
     console.log("Picking new gateway from " + gatewayText + "...");
     await page.focus("#gateway");
-    // Allow console.log from page.evaluate
-    page.on('console', consoleObj => console.log(consoleObj.text()));
-    await page.evaluate((gatewayTextIncludes) => {
+
+    let gw = await page.evaluate((gatewayTextIncludes) => {
         const example = document.querySelector('#gateway');
         const example_options = example.querySelectorAll('option');
         const selected_option = [...example_options].find(option => option.text.includes(gatewayTextIncludes));
-        
-        console.log("Picked gateway " + selected_option.text + ".");
 
         selected_option.selected = true;
+        return selected_option.text
     }, gatewayText);
-    // Disallow console.log from page.evaluate
-    page.on('console', () => {});
+
+    console.log("Picked gateway " + gw + ".");
 
     await page.waitFor(1000);
     console.log("Saving rule...");
